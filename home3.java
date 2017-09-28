9ae8170f68e5a522504b99654abbb48fb8f26fd3
LincolnFrederico <lcnfred333@gmail.com>
2017-09-28 13:34:30 -0300

    Arquivos Java
    Arquivos Java pasta "Com"

---
+++ home3.java
@@ -0,0 +1,13 @@
+package com.lcn_fredericohotmail.lcn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home3);
+    }
+}
